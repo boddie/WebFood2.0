7b340971a0ce1070766271d8f32cd15ebd279ad5
james <jdboddie@live.com>
2014-11-12 22:30:13 -0600

    Added cancellOrder and getLocationOrders for nodejs/Mongo and unit test cases for them

--- data/order.js
+++ data/order.js
@@ -1,16 +1,15 @@
 var q = require('q');
-var debug = require('debug')('webfood:userdao');
+var debug = require('debug')('webfood:orderdoa');
 
 module.exports = function(db) {
 	var Order = db.collection('orders');
 	
-	var createOrder = function (id, location, price, entree, bev, sides, other) {
+	var createOrder = function (location, price, entree, bev, sides, other) {
 		var deferred = q.defer();
 		
 		debug("Creating order");
 		
 		Order.insert({
-			id: id,
 			location: location,
 			price: price,
 			entree: entree,
@@ -30,6 +29,8 @@ module.exports = function(db) {
 	var closeOrder = function (id) {
 	
 		var deferred = q.defer();
+		
+		debug("Creating order");
 
 		Order.update({ id: id }, { $set: { status: 'closed' } }, function(err, documents) {
 			if (err) debug('err: ', err);
@@ -40,121 +41,40 @@ module.exports = function(db) {
 		return deferred.promise;
 	};
 	
-	return {
-		createOrder : createOrder,
-		closeOrder : closeOrder
-	};
-};
-
-/*
-var MongoClient = require('mongodb').MongoClient;
-
-module.exports.createOrder = function(id, location, price, entree, bev, sides, other) {
-	MongoClient.connect(
-		'mongodb://boddie:password1@ds051110.mongolab.com:51110/se329p3',
-		function(err, connection) {
-			// Uses customer collection within se329p3 database
-			var collection = connection.collection('orders');
-			
-			collection.insert({
-				id: id,
-				location: location,
-				price: price,
-				entree: entree,
-				beverage: bev,
-				sides: sides,
-				other: other,
-				status: 'new'
-				}, function(err, count) {
-					Console.Log("Count: ", count);
-					connection.close();
-					return count;
-			});
-		}
-	);
-};
-
-
-/*
-var db = require('./connection')(function (err, db) {
-	var Order = require('mongolia').model(db, 'oders');
+	var cancellOrder = function (id) {
 	
-	module.exports.createOrder = function(id, location, price, entree, bev, sides, other) {
 		var deferred = q.defer();
-		Order.mongo('insert', {
-			id: id,
-			location: location,
-			price: price,
-			entree: entree,
-			beverage: bev,
-			sides: sides,
-			other: other,
-			status: 'new'
-		}, function(err, documents) {
-			if (err) debug('err: ', err);
-			else debug('Create order success');
-			deferred.resolve(documents);
-		});
+		
+		debug("Cancelling order");
 
-		return deferred.promise;
-	};
-	
-	module.exports.closeOrder = function(id) {
-		var deferred = q.defer();
-		Order.mongo('update', { id: id },
-			{$set: { status: 'closed' }
-		}, function(err, documents) {
-			if (err) debug('err: ', err);
-			else debug('Close order success');
-			deferred.resolve(documents);
-		});
-
-		return deferred.promise;
-	};
-	
-	module.exports.cancelOrder = function(id) {
-		var deferred = q.defer();
-		Order.mongo('update', { id: id },
-			{$set: { status: 'cancelled' }
-		}, function(err, documents) {
+		Order.update({ id: id }, { $set: { status: 'cancelled' } }, function(err, documents) {
 			if (err) debug('err: ', err);
 			else debug('Cancel order success');
 			deferred.resolve(documents);
 		});
-
+		
 		return deferred.promise;
 	};
 	
-	module.exports.getOrderByID = function(id) {
-		var deferred = q.defer();
-
-		Order.findOne({ id: id }, function (err, result) {
-			if (err) {
-				debug('findOne err: ', err);
-				deferred.reject(err);
-			}
-			else {
-				deferred.resolve(result);
-			}
-		});
-
-		return deferred.promise;
-	};
+	var getLocationOrders = function (location) {
 	
-	module.exports.getLocationOrders = function(location) {
 		var deferred = q.defer();
 		
-		Order.find({ location: location }).toArray(function(err, results) {
-			if (err) {
-				debug('find err: ', err);
-				deferred.reject(err);
-			}
-			else {
-				deferred.resolve(results);
-			}
+		debug("Getting locations for:", location);
+
+		Order.find({ 'location': location }).toArray(function(err, documents) {
+			if (err) debug('err: ', err);
+			else debug('Pulled orders successfully');
+			deferred.resolve(documents);
 		});
 		
 		return deferred.promise;
 	};
 	
-});*/
+	return {
+		createOrder : createOrder,
+		closeOrder : closeOrder,
+		cancellOrder : cancellOrder,
+		getLocationOrders : getLocationOrders
+	};
+};
